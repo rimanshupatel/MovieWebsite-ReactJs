4877ecb5a2834a080e2aa0eb0f909fe7d7e2d717
rimanshupatel <rimanshupatel1@gmail.com>
2024-05-08 22:14:55 +0530

    adding new features that show error when user enter wrong keywords and improvement in some other components

--- src/components/Navbar/Search.jsx
+++ src/components/Navbar/Search.jsx
@@ -1,56 +1,65 @@
 import { useState } from "react";
 import { UseGloballyContext } from "../../Context/Context";
 export default function Search() {
-  const { setHandleSearch } = UseGloballyContext();
-  const [searchVal, setSearchVal] = useState("");
+  const { setHandleSearch, error } = UseGloballyContext();
+  // const [searchVal, setSearchVal] = useState("");
   let handleChange = (e) => {
-    setSearchVal(e.target.value);
+    setHandleSearch(e.target.value);
+    // setHandleSearch(searchVal);
   };
   let handleClick = (event) => {
     event.preventDefault();
-    setHandleSearch(searchVal);
+    // setHandleSearch(searchVal);
   };
   return (
-    <form className="max-w-md ">
-      <label
-        htmlFor="default-search"
-        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-      >
-        Search
-      </label>
-      <div className="relative">
-        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-          <svg
-            className="w-4 h-4 text-gray-500 dark:text-gray-400"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 20"
+    <div>
+      <div>
+        <form className="max-w-md ">
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
           >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            Search
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
+            </div>
+            <input
+              type="search"
+              id="default-search"
+              onChange={handleChange}
+              className="block w-96 py-3 px-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
+              placeholder="Search movies, series and more..."
+              required
             />
-          </svg>
-        </div>
-        <input
-          type="search"
-          id="default-search"
-          onChange={handleChange}
-          className="block w-96 py-3 px-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 "
-          placeholder="Search movies, series and more..."
-          required
-        />
-        <button
-          onClick={handleClick}
-          className="text-white absolute end-2.5 bottom-1 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Search
-        </button>
+            <button
+              onClick={handleClick}
+              className="text-white absolute end-2.5 bottom-1 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Search
+            </button>
+          </div>
+        </form>
       </div>
-    </form>
+
+      <div>
+        <p>{error.show && error.msg}</p>
+      </div>
+    </div>
   );
 }
